d642e836a322a131352ee749496e5684f8579987
masoft <morseck00@gmail.com>
2022-01-31 18:39:01 -0800

    init commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB53Fw2H1Q6Q_Iaehk_5PU3l8OUQ-nek_M",
+    authDomain: "crudangular13firebase.firebaseapp.com",
+    projectId: "crudangular13firebase",
+    storageBucket: "crudangular13firebase.appspot.com",
+    messagingSenderId: "37296843266",
+    appId: "1:37296843266:web:78a5a8a1dc8f566a04951b"
+  }
 };
 
 /*
